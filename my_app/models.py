42b06a94230ae21aeefb60e63994a4466ce2b332
Ivan Rudnev <rudnev.vanya@gmail.com>
2017-05-28 16:57:47 +0300

    Device model

--- my_app/models.py
+++ my_app/models.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
+import datetime
 
-# Create your models here.
+
+
+
+class DeviceManager(models.Manager):
+	def new(self):
+		return self.order_by('-added_at')
+	def cheaper(self):
+		return self.order_by('price')
+	def expensive(self):
+		return self.order_by('-price')
+
+
+class Device(models.Model):
+	title = models.CharField (max_length = 128)
+	description = models.TextField()
+	added_at = models.DateTimeField(default = datetime.datetime.now)
+	price = models.DecimalField(default=0)
+	objects = DeviceManager()
